579daccd689f7709da7f52718d71cd5f9c7c70be
DonatellaC <donatella.carta@mailbox.org>
2022-02-23 20:26:22 +0100

    added functionality heart and reset color

--- 05-heart-customiser-api/index.js
+++ 05-heart-customiser-api/index.js
@@ -1,5 +1,10 @@
 const body = document.body;
+const heart = document.getElementById("heart");
 document.getElementById("btn").addEventListener("click", changeColor);
+document.getElementById("btn-reset").addEventListener("click", resetColor);
+document
+  .getElementById("btn-heart")
+  .addEventListener("click", changeColorHeart);
 
 async function changeColor() {
   // 1. Call Scrimba's color API (https://apis.scrimba.com/hexcolors/) to retrieve one random color.
@@ -9,4 +14,17 @@ async function changeColor() {
   const colorsBody = await response.json();
   const bodyBackgroundColor = colorsBody.colors[0].value;
   body.style.setProperty("background-color", bodyBackgroundColor);
+}
+
+async function changeColorHeart() {
+  const url = "https://apis.scrimba.com/hexcolors/?count=1";
+  const response = await fetch(url);
+  const colorsHeart = await response.json();
+  const heartBackgroundColor = colorsHeart.colors[0].value;
+  heart.style.setProperty("--white", heartBackgroundColor);
+}
+
+function resetColor() {
+  body.style.setProperty("background-color", "#c7b9ff");
+  heart.style.setProperty("--white", "#fff");
 }
