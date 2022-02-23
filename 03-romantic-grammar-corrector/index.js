a2576f0a020ef93beadbb948c09a741a4965f34c
DonatellaC <donatella.carta@mailbox.org>
2022-02-23 16:43:39 +0100

    added functionality romantic grammar corrector

--- 03-romantic-grammar-corrector/index.js
+++ 03-romantic-grammar-corrector/index.js
@@ -1,7 +1,15 @@
+const message = document.getElementById("paragraph");
+const paragraphMessage = message.textContent;
+const firstLetter = paragraphMessage.charAt(0).toUpperCase();
+const remainderLetters = paragraphMessage.slice(1).toLowerCase();
+
 document.getElementById("btn").addEventListener("click", correct);
 
 function correct() {
   // Write the JavaScript to grab the message from the paragraph and correct its capitalisation.
   // It should read "Valentine's"
   // Render the corrected message to the DOM.
+
+  const correctedMessage = firstLetter + remainderLetters;
+  message.textContent = correctedMessage;
 }
