41ed738ede6bacac5afcfdfc5c3515d572b891b9
DonatellaC <donatella.carta@mailbox.org>
2022-02-23 19:50:47 +0100

    added functionality heart customiser api

--- 05-heart-customiser-api/index.js
+++ 05-heart-customiser-api/index.js
@@ -4,4 +4,9 @@ document.getElementById("btn").addEventListener("click", changeColor);
 async function changeColor() {
   // 1. Call Scrimba's color API (https://apis.scrimba.com/hexcolors/) to retrieve one random color.
   // 2. Use that color to update the background color of the body.
+  const url = "https://apis.scrimba.com/hexcolors/?count=1";
+  const response = await fetch(url);
+  const colorsBody = await response.json();
+  const bodyBackgroundColor = colorsBody.colors[0].value;
+  body.style.setProperty("background-color", bodyBackgroundColor);
 }
